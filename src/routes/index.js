0ca779cfdde774c5743cac8108011a2dbbd96fc3
rodrigocichetto-exacta <rodrigocichetto@exactaworks.com.br>
2019-12-17 13:56:50 -0300

    feat: add root import

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import SignIn from '~/pages/SignIn';
+import Dashboard from '~/pages/Dashboard';
+
+const Routes = () => (
+  <Switch>
+    <Route path="/" exact component={SignIn} />
+
+    <Route path="/dashboard" component={Dashboard} />
+  </Switch>
+);
+
+export default Routes;
